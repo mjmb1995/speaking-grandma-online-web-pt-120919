9947146c954a48cc5202f3b30ec2defbef3ca6f4
Mark Bihag <markbihag330@gmail.com>
2020-01-02 07:25:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif string == string.upcase
+  elsif string == string.upcase && string != "I LOVE YOU GRANDMA!"
     return "NO, NOT SINCE 1938!"
   elseif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
